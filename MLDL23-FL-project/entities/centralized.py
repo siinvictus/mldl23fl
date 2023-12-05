559f67e997d622b1f0f40405457be0964ab26146
Silva Bashllari <88505317+siinvictus@users.noreply.github.com>
2023-12-05 11:31:02 +0100

    Add files via upload

---
+++ MLDL23-FL-project/entities/centralized.py
@@ -0,0 +1,168 @@
+import json
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import os
+
+import os
+import sys
+path = os.getcwd()
+if 'kaggle' not in path:
+    from datasets.femnist import Femnist
+else:
+    sys.path.append('datasets')
+    from femnist import Femnist
+
+
+
+IMAGE_SIZE = 28
+
+
+
+
+
+
+class Centralized:
+
+    def __init__(self, data_path, model, optimizer, criterion, device, transforms):
+        self.path = data_path
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        self.transforms = transforms
+
+    def n_classes(self, batch):
+        return batch['class'].unique().shape[0]
+    def data_parser(self, df):
+        """
+        takes a dataframe sorted by writers and unpacks the data
+        return: dataframe with two columns [img,class]
+        """
+        transpose = df.T
+        out_dict = dict()
+        idx = 0
+        for _, row in transpose.iterrows():
+            row_x = row.x
+            for i, val in enumerate(row.y):
+                out_dict[idx] = [np.array(row_x[i], dtype=np.float16), val]
+                idx += 1
+        out_df = pd.DataFrame(out_dict).T
+        out_df = out_df.rename(columns={0: 'img', 1: 'class'})
+        return out_df
+
+    def get_data(self):
+        df = pd.DataFrame()
+        print('loading files.....')
+        for dirname, _, filenames in os.walk(self.path):
+            for filename in filenames:
+                #print(filename)
+                data = json.load(open(os.path.join(dirname, filename)))
+
+                temp_df = pd.DataFrame(data['user_data'])
+                temp_df = temp_df.reset_index(drop=True)
+                df = pd.concat([df, temp_df], axis=1)  # ignore_index=True
+        df = df.rename(index={0: "x", 1: "y"})
+        return df
+
+    def train_test_tensors(self, batch):
+        convert_tensor = transforms.ToTensor()
+        X_train, X_val, y_train, y_val = train_test_split(batch['img'], batch['class'], test_size=0.2, random_state=42)
+        torch_train = Femnist(
+            {'x': X_train.tolist(), 'y': y_train.tolist()},
+            self.transforms, '')
+        torch_test = Femnist(
+            {'x': X_val.tolist(), 'y': y_val.tolist()},
+            self.transforms, '')
+
+        return torch_train, torch_test
+
+    def training(self, torch_train):
+
+        train_loader = DataLoader(torch_train, batch_size=64, shuffle=True)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        for epoch in range(5):  # loop over the dataset multiple times
+            running_loss = 0.0
+            for i, data in enumerate(train_loader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                inputs, labels = inputs.cuda(), labels.cuda()
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:  # print every 2000 mini-batches
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+        print('Finished Training')
+
+    def accuracy_of_model(self, val_loader):
+        correct = 0
+        total = 0
+        # since we're not training, we don't need to calculate the gradients for our outputs
+        with torch.no_grad():
+            for data in val_loader:
+                images, labels = data
+                images, labels = images.cuda(), labels.cuda()
+                # calculate outputs by running images through the network
+                outputs = self.model(images)
+                # the class with the highest energy is what we choose as prediction
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+    """
+    def accuracy_for_class(self, val_loader, classes):
+        # prepare to count predictions for each class
+        correct_pred = {classname: 0 for classname in range(classes)}
+        total_pred = {classname: 0 for classname in range(classes)}
+
+        # again no gradients needed
+        with torch.no_grad():
+            for data in val_loader:
+                images, labels = data
+                images, labels = images.cuda(), labels.cuda()
+                outputs = self.model(images)
+                _, predictions = torch.max(outputs, 1)
+                # collect the correct predictions for each class
+                for label, prediction in zip(labels, predictions):
+                    if label == prediction:
+                        correct_pred[classes[label]] += 1
+                    total_pred[classes[label]] += 1
+
+        # print accuracy for each class
+        for classname, correct_count in correct_pred.items():
+            accuracy = 100 * float(correct_count) / total_pred[classname]
+            print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+    """
+    def pipeline(self):
+        print('loading data')
+        out_df = self.get_data()
+        print('preprocessing')
+        # dataframe of the dataset
+        df = self.data_parser(out_df)
+        del out_df
+        print('Done')
+        #n_classes = self.n_classes(df)
+        # train and test tensors
+        torch_train, torch_test = self.train_test_tensors(df)
+        print('Training')
+        self.training(torch_train)
+        print('Done.')
+        # printing accuracy
+        val_loader = DataLoader(torch_test, batch_size=64, shuffle=False)
+        print('Validating')
+        self.accuracy_of_model(val_loader)
+
---
+++ MLDL23-FL-project/main.py
@@ -0,0 +1,194 @@
+import os
+import json
+from collections import defaultdict
+
+import torch
+import random
+
+import numpy as np
+from torch import nn
+from torchvision.models import resnet18
+import sys
+
+sys.path.append('datasets')
+
+
+from entities.client import Client
+
+from entities.centralized import Centralized
+from torchvision import transforms
+from entities.server import Server
+from utils.args import get_parser
+
+from models.cnn import CNN
+from utils.stream_metrics import StreamSegMetrics, StreamClsMetrics
+
+
+def set_seed(random_seed):
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_dataset_num_classes(dataset):
+    if dataset == 'femnist':
+        return 62
+    else:
+        raise NotImplementedError
+
+
+def model_init(args):
+    """
+    if args.model == 'deeplabv3_mobilenetv2':
+        return deeplabv3_mobilenetv2(num_classes=get_dataset_num_classes(args.dataset))
+    """
+    if args.model == 'resnet18':
+        model = resnet18()
+        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        model.fc = nn.Linear(in_features=512, out_features=get_dataset_num_classes(args.dataset))
+        return model
+
+    if args.model == 'cnn':
+
+        model = CNN(get_dataset_num_classes('femnist'))
+        return model
+    else:
+        raise NotImplementedError
+
+
+def get_transforms(args):
+    # TODO: test your data augmentation by changing the transforms here!
+    if args.model == 'cnn' or args.model == 'resnet18':
+
+        train_transforms = sstr.Compose([
+            transforms.ToTensor(),
+            nptr.Normalize((0.5,), (0.5,)),
+        ])
+        test_transforms = sstr.Compose([
+            nptr.ToTensor(),
+            nptr.Normalize((0.5,), (0.5,)),
+        ])
+
+
+    else:
+        raise NotImplementedError
+    return train_transforms, test_transforms
+
+
+def read_femnist_dir(data_dir):
+    data = defaultdict(lambda: {})
+    files = os.listdir(data_dir)
+    files = [f for f in files if f.endswith('.json')]
+    for f in files:
+        file_path = os.path.join(data_dir, f)
+        with open(file_path, 'r') as inf:
+            cdata = json.load(inf)
+        data.update(cdata['user_data'])
+    return data
+
+
+def read_femnist_data(train_data_dir, test_data_dir):
+    return read_femnist_dir(train_data_dir), read_femnist_dir(test_data_dir)
+
+
+def get_datasets(args):
+    train_datasets = []
+    train_transforms, test_transforms = get_transforms(args)
+
+    # elif args.dataset == 'femnist':
+    niid = args.niid
+    train_data_dir = os.path.join('data', 'femnist', 'data', 'niid' if niid else 'iid', 'train')
+    test_data_dir = os.path.join('data', 'femnist', 'data', 'niid' if niid else 'iid', 'test')
+    train_data, test_data = read_femnist_data(train_data_dir, test_data_dir)
+
+    train_transforms, test_transforms = get_transforms(args)
+
+    train_datasets, test_datasets = [], []
+
+    for user, data in train_data.items():
+        train_datasets.append(Femnist(data, train_transforms, user))
+    for user, data in test_data.items():
+        test_datasets.append(Femnist(data, test_transforms, user))
+
+    return train_datasets, test_datasets
+
+
+def set_metrics(args):
+    num_classes = get_dataset_num_classes(args.dataset)
+    if args.model == 'deeplabv3_mobilenetv2':
+        metrics = {
+            'eval_train': StreamSegMetrics(num_classes, 'eval_train'),
+            'test_same_dom': StreamSegMetrics(num_classes, 'test_same_dom'),
+            'test_diff_dom': StreamSegMetrics(num_classes, 'test_diff_dom')
+        }
+    elif args.model == 'resnet18' or args.model == 'cnn':
+        metrics = {
+            'eval_train': StreamClsMetrics(num_classes, 'eval_train'),
+            'test': StreamClsMetrics(num_classes, 'test')
+        }
+    else:
+        raise NotImplementedError
+    return metrics
+
+
+def gen_clients(args, train_datasets, test_datasets, model):
+    clients = [[], []]
+    for i, datasets in enumerate([train_datasets, test_datasets]):
+        for ds in datasets:
+            clients[i].append(Client(args, ds, model, test_client=i == 1))
+    return clients[0], clients[1]
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    set_seed(args.seed)
+
+    print('Initializing model...')
+    model = model_init(args)
+    model.cuda()
+    print('Done.')
+
+    if args.federated:
+        print('Generate datasets...')
+        train_datasets, test_datasets = get_datasets(args)
+        print('Done.')
+
+        metrics = set_metrics(args)
+        print(metrics)
+
+        """
+        train_clients, test_clients = gen_clients(args, train_datasets, test_datasets, model)
+        server = Server(args, train_clients, test_clients, model, metrics)
+        server.train()
+        """
+    else:
+        data_path = os.path.join('data', 'femnist', 'data', 'all_data')
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        optimizer = torch.optim.SGD(model.parameters(),
+                                    lr=0.001)  # define loss function criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss()
+        data_transform = transforms.ToTensor()
+        centralized = Centralized(data_path=data_path, model=model,
+                                  optimizer=optimizer, criterion=criterion,
+                                  device=device, transforms=data_transform)
+        centralized.pipeline()
+
+
+if __name__ == '__main__':
+    path = os.getcwd()
+    if 'kaggle' not in path:
+        import datasets.ss_transforms as sstr
+        import datasets.np_transforms as nptr
+        from datasets.femnist import Femnist
+    else:
+        sys.path.append('datasets')
+        import ss_transforms as sstr
+        import np_transforms as nptr
+        from femnist import Femnist
+
+    main()
