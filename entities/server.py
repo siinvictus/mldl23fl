58db4b0d9fcbab675ae066e1e7427e347890b6f9
Silva Bashllari <88505317+siinvictus@users.noreply.github.com>
2024-02-02 11:14:02 +0100

    Update server.py
    add mean sparsity column

--- entities/server.py
+++ entities/server.py
@@ -193,7 +193,7 @@ class Server:
             list_p10 = [1/n30perc] * n30perc
             list_p90 = [1/(len(self.train_clients)-n30perc)] * (len(self.train_clients)-n30perc)
             
-        test_accuracyp, train_accuracyp = [], []
+        test_accuracyp, train_accuracyp, meanslist = [], [], []
 
         for r in range(self.args.num_rounds):
             # our addition
@@ -228,6 +228,7 @@ class Server:
             test_accuracy = self.test(aggregated_params)
             test_accuracyp.append(test_accuracy)
             train_accuracyp.append(train_accuracy)
+            meanslist.append(mean_sparsity)
             print(f"Test Accuracy for round {r + 1}: {test_accuracy:.4f}")
             print(f"Mean sparsity for round {r + 1} (only for pruning): {mean_sparsity:.4f}")
 
@@ -237,7 +238,7 @@ class Server:
             train_csv = pd.DataFrame(train_dict)
             train_csv.to_csv(f'Federated_Non-IID:{self.args.niid}_LocalEpochs:{self.args.num_epochs}_Lr:{self.args.lr}_momentum:{self.args.m}_wd:{self.args.wd}_batchSize:{self.args.bs}.csv', index = False)
         else:
-            train_dict = {'Epochs': np.array(range(self.args.num_rounds)),'Train accuracy': np.array(train_accuracyp), 'Test accuracy': np.array(test_accuracyp)}
+            train_dict = {'Epochs': np.array(range(self.args.num_rounds)),'Train accuracy': np.array(train_accuracyp), 'Test accuracy': np.array(test_accuracyp),'Mean Sparsity': np.array(meanslist)}
             train_csv = pd.DataFrame(train_dict)
             train_csv.to_csv(f'Federated_Non-IID:{self.args.niid}_clientSelection:{self.args.client_select}_powerOfChoicesM:{self.args.power_of_choice_m}_prune:{self.args.prune}_conv:{self.args.conv}_linear:{self.args.linear}_strucured{self.args.structured}_amount_prune:{self.args.amount_prune}.csv', index = False)
 
